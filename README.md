77462c033902cccaf11089ccb89e8f82c2c9c283
Florent Uzio <fuzio@ripple.com>
2024-02-12 13:23:14 +0400

    commands

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# dsm-zigbee-commands-install
+# DSM Commands to install Zigbee USB drivers
+
+I found the modules to install on https://github.com/robertklep/dsm7-usb-serial-drivers/blob/main/README.md.
+
+Also a useful comment to use the "raw" file in the wget commands:
+
+https://github.com/robertklep/dsm7-usb-serial-drivers/issues/110#issuecomment-1800239978
---
+++ commands.sh
@@ -0,0 +1,33 @@
+# Website
+# https://web.archive.org/web/20211027124225/http://www.jadahl.com/iperf-arp-scan/DSM_7.0/
+
+# compiled USB Modules
+
+sudo su
+
+modprobe usbserial
+modprobe ftdi_sio
+modprobe cdc-acm
+
+exit
+
+cd /lib/modules/
+
+sudo wget https://github.com/robertklep/dsm7-usb-serial-drivers/raw/main/modules/r1000/dsm-7.2/ch341.ko
+sudo insmod /lib/modules/ch341.ko
+sudo wget https://github.com/robertklep/dsm7-usb-serial-drivers/raw/main/modules/r1000/dsm-7.2/cp210x.ko
+sudo insmod /lib/modules/cp210x.ko
+sudo wget https://github.com/robertklep/dsm7-usb-serial-drivers/raw/main/modules/r1000/dsm-7.2/pl2303.ko
+sudo insmod /lib/modules/pl2303.ko
+sudo wget https://github.com/robertklep/dsm7-usb-serial-drivers/raw/main/modules/r1000/dsm-7.2/ti_usb_3410_5052.ko
+sudo insmod /lib/modules/ti_usb_3410_5052.ko
+
+# Configure an autoload script
+
+cd  /usr/local/etc/rc.d/
+sudo wget https://github.com/robertklep/dsm7-usb-serial-drivers/raw/main/usb-serial-drivers.sh
+sudo chmod +x usb-serial-drivers.sh
+
+# Make sure your devices are recongnized
+
+sudo /usr/local/etc/rc.d/usb-serial-drivers.sh start
